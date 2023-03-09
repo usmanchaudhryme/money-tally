c12d7b545f9a47fb3cfca4819ef091cdb4a45a3c
Usman Chaudhry <uchaudhry@jovoto.com>
2023-03-09 11:52:29 +0500

    Update UI styling
    Style the UI, having an input that shall be used to get the values from user, while having another disabled input to show the sum.

--- src/lib/components/MoneyCalculatorForm.svelte
+++ src/lib/components/MoneyCalculatorForm.svelte
@@ -1,1 +1,47 @@
-<div class="container">Calculator Goes Here</div>
+<div class="container">
+	<div class="row">
+		<input />
+	</div>
+	<div class="row result">
+		SUM: <input disabled />
+	</div>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		max-width: 300px;
+		justify-content: center;
+		position: relative;
+		top: 50%;
+		left: 50%;
+		transform: translate(-58%);
+	}
+	.row {
+		border: 2px solid darkgray;
+		width: 296px;
+		display: flex;
+		justify-content: flex-end;
+		padding-left: 2rem;
+	}
+	.row.result {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		font-size: 1.5rem;
+	}
+	.row:not(:first-child) {
+		border-top: none;
+	}
+	input {
+		margin: 1rem;
+		text-align: right;
+		padding-right: 0.5rem;
+		width: 180px;
+		height: 2rem;
+		font-size: 1.5rem;
+		font-weight: 400;
+		border: 2px solid darkgray;
+	}
+</style>
